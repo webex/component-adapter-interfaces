3aa6bbda6fa9c87d7c581543ffd7ac3c4db89fc3
Arash Koushkebaghi <arash.koush@gmail.com>
2019-10-22 11:25:55 -0700

    build(rollup): configure rollup

---
+++ rollup.config.js
@@ -0,0 +1,22 @@
+import babel from "rollup-plugin-babel";
+import resolve from "rollup-plugin-node-resolve";
+import commonJS from "rollup-plugin-commonjs";
+
+const output = (name, format) => ({
+  name,
+  file: `dist/webexComponentAdapterInterfaces.${format}.js`,
+  format,
+  sourcemap: true
+});
+
+export default [
+  {
+    input: "interfaces/index.js",
+    output: [
+      output("WebexComponentAdapterInterfaces", "cjs"),
+      output("UMDWebexComponentAdapterInterfaces", "umd"),
+      output("ESMWebexComponentAdapterInterfaces", "esm")
+    ],
+    plugins: [resolve(), babel(), commonJS()]
+  }
+];
