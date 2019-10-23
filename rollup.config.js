4c2b73fe9e05058c0e42713003e088b2df0ab3d8
Arash Koushkebaghi <arash.koush@gmail.com>
2019-10-23 11:11:51 -0700

    build(rollup): prettify rollup configuration

--- rollup.config.js
+++ rollup.config.js
@@ -1,22 +1,22 @@
-import babel from "rollup-plugin-babel";
-import resolve from "rollup-plugin-node-resolve";
-import commonJS from "rollup-plugin-commonjs";
+import babel from 'rollup-plugin-babel';
+import resolve from 'rollup-plugin-node-resolve';
+import commonJS from 'rollup-plugin-commonjs';
 
 const output = (name, format) => ({
   name,
   file: `dist/webexComponentAdapterInterfaces.${format}.js`,
   format,
-  sourcemap: true
+  sourcemap: true,
 });
 
 export default [
   {
-    input: "interfaces/index.js",
+    input: 'src/index.js',
     output: [
-      output("WebexComponentAdapterInterfaces", "cjs"),
-      output("UMDWebexComponentAdapterInterfaces", "umd"),
-      output("ESMWebexComponentAdapterInterfaces", "esm")
+      output('WebexComponentAdapterInterfaces', 'cjs'),
+      output('UMDWebexComponentAdapterInterfaces', 'umd'),
+      output('ESMWebexComponentAdapterInterfaces', 'esm'),
     ],
-    plugins: [resolve(), babel(), commonJS()]
-  }
+    plugins: [resolve(), babel(), commonJS()],
+  },
 ];
