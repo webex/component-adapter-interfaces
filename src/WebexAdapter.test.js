a387d038fee4b6e537f71088634b23c201058c2e
Arash Koushkebaghi <arash.koush@gmail.com>
2019-10-23 11:07:09 -0700

    build(src): rename interfaces to src

--- src/WebexAdapter.test.js
+++ src/WebexAdapter.test.js
@@ -1,14 +1,14 @@
-import WebexAdapter from "./WebexAdapter";
+import WebexAdapter from './WebexAdapter';
 
-describe("Webex Adapter Interface", () => {
+describe('Webex Adapter Interface', () => {
   let webexAdapter, datasource;
 
   beforeEach(() => {
-    datasource = "WebexAdapterDataSource";
+    datasource = 'WebexAdapterDataSource';
     webexAdapter = new WebexAdapter(datasource);
   });
 
-  test("WebexAdapter stores the data source", () => {
+  test('WebexAdapter stores the data source', () => {
     expect(webexAdapter.datasource).toBe(datasource);
   });
 
