dab06ca2ec55f7860eb623151a65a06b0bee13c2
lalli-flores <lalli.flores@gmail.com>
2019-10-25 15:29:30 -0700

    fix(ActivitiesAdapter): export Activities adapter

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
 export {default as WebexAdapter} from './WebexAdapter';
+export {default as ActivitiesAdapter} from './ActivitiesAdapter';
 export {default as PeopleAdapter} from './PeopleAdapter';
 export {default as RoomsAdapter} from './RoomsAdapter';
