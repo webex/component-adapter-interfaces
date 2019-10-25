cb90b52a882e7c84612bec9f639e1af4f042b8ee
lalli-flores <lalli.flores@gmail.com>
2019-10-25 12:14:14 -0400

    feat(MeetingsAdapter): define adapter

---
+++ src/MeetingsAdapter.test.js
@@ -0,0 +1,33 @@
+import {isObservable} from 'rxjs';
+
+import MeetingsAdapter from './MeetingsAdapter';
+
+describe('Meetings Adapter Interface', () => {
+  let meetingsAdapter;
+
+  beforeEach(() => {
+    meetingsAdapter = new MeetingsAdapter();
+  });
+
+  test('getMeeting() returns an observable', () => {
+    expect(isObservable(meetingsAdapter.getMeeting())).toBeTruthy();
+  });
+
+  test('getMeeting() errors because it needs to be defined', (done) => {
+    meetingsAdapter.getMeeting('msgID').subscribe(
+      () => {},
+      (error) => {
+        expect(error.message).toBe('getMeeting(ID) must be defined in MeetingsAdapter');
+        done();
+      }
+    );
+  });
+
+  test('meetingControls property exists', () => {
+    expect(meetingsAdapter).toHaveProperty('meetingControls');
+  });
+
+  afterEach(() => {
+    meetingsAdapter = null;
+  });
+});
