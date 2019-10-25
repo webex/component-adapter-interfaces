91d6cb89720b4561ef6f59d471649ee06edc2266
Adam Weeks <adweeks@cisco.com>
2019-10-25 17:05:03 -0400

    docs(WebexAdapter): add base jsdoc for class

--- src/WebexAdapter.js
+++ src/WebexAdapter.js
@@ -1,3 +1,6 @@
+/**
+ * This is a base class that defines the interface that other adapter interfaces extend.
+ */
 export default class WebexAdapter {
   /**
    * Creates a new instance of the WebexAdapter.
