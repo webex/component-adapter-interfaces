ad4060a5855c963f7a1c14d99b8cb0a0e0d35184
Arash Koushkebaghi <arash.koush@gmail.com>
2020-02-27 11:21:27 -0800

    build(release): remove cjs and umd files to publish

--- release.config.js
+++ release.config.js
@@ -6,45 +6,37 @@
  */
 module.exports = {
   plugins: [
-    "@semantic-release/commit-analyzer",
+    '@semantic-release/commit-analyzer',
     [
-      "@semantic-release/release-notes-generator",
+      '@semantic-release/release-notes-generator',
       {
         writerOpts: {
-          commitsSort: ["subject", "scope"]
-        }
-      }
+          commitsSort: ['subject', 'scope'],
+        },
+      },
     ],
-    "@semantic-release/changelog",
-    "@semantic-release/npm",
+    '@semantic-release/changelog',
+    '@semantic-release/npm',
     [
-      "@semantic-release/git",
+      '@semantic-release/git',
       {
-        assets: ["CHANGELOG.md", "package.json", "package-lock.json"]
-      }
+        assets: ['CHANGELOG.md', 'package.json', 'package-lock.json'],
+      },
     ],
     [
-      "@semantic-release/github",
+      '@semantic-release/github',
       {
         assets: [
           {
-            path: "package.json",
-            label: "Package JSON"
+            path: 'package.json',
+            label: 'Package JSON',
           },
           {
-            path: "dist/webexComponentAdapterInterfaces.cjs.js",
-            label: "Common JS distribution"
+            path: 'dist/webexComponentAdapterInterfaces.esm.js',
+            label: 'ECMAScript module JS distribution',
           },
-          {
-            path: "dist/webexComponentAdapterInterfaces.umd.js",
-            label: "Universal Module Definition JS distribution"
-          },
-          {
-            path: "dist/webexComponentAdapterInterfaces.esm.js",
-            label: "ECMAScript module JS distribution"
-          }
-        ]
-      }
-    ]
-  ]
+        ],
+      },
+    ],
+  ],
 };
