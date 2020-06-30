71418903c44f2d662194edce1f172223f9abff0b
Zixi Xu (zixxu) <zixxu@cisco.com>
2020-06-30 10:19:12 -0700

    feat(memberships): add memberships adapter interface

---
+++ src/MembershipsAdapter.js
@@ -0,0 +1,49 @@
+import {throwError} from 'rxjs';
+
+import WebexAdapter from './WebexAdapter';
+
+/**
+ * A Membership object with details about the member.
+ *
+ * @typedef {Object} Membership
+ * @property {string}             ID               The member identifier.
+ * @property {string}             destinationID    ID of the destination for which to get members.
+ * @property {string}             destinationType  type of the membership destination.
+ * @property {Array.<Object>}     members          An array of members information.
+ */
+
+/**
+ * Enum for types of destinations.
+ *
+ * @readonly
+ * @enum {string}
+ */
+export const DestinationType = {
+  ROOM: 'room',
+  MEETING: 'meeting',
+};
+
+/**
+ * This is a base class that defines the interface that maps membership data.
+ * Developers that want to extend `MembershipsAdapter` must implement all of its methods,
+ * adhering to the exact parameters and structure of the returned objects.
+ *
+ * @interface
+ */
+export default class MembershipsAdapter extends WebexAdapter {
+  /**
+   * Returns an observable that emits a Membership object.
+   * Whenever there is an update to the membership, the observable
+   * will emit a new updated Membership object, if datasource permits.
+   *
+   * @param {string} destinationID  ID of the destination for which to get members.
+   * @param {DestinationType} destinationType  type of the membership destination.
+   * @returns {Observable.<Membership>}
+   * @memberof MembershipAdapter
+   */
+  getMembersFromDestination(destinationID, destinationType) {
+    return throwError(
+      new Error('getMembersFromDestination(destinationID, destinationType) must be defined in MembershipsAdapter')
+    );
+  }
+}
