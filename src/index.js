30b76cce9c64011844e2d19d4d34d52beaa7c62c
Karina Sigartau <karina.sigartau0711@gmail.com>
2021-06-30 09:40:10 +0300

    fix(MeetingsAdapter): export MediaPermissions

--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
 export {default as WebexAdapter} from './WebexAdapter';
 export {default as ActivitiesAdapter} from './ActivitiesAdapter';
-export {default as MeetingsAdapter, MeetingControlState, MeetingState} from './MeetingsAdapter';
+export {
+  default as MeetingsAdapter,
+  MeetingControlState,
+  MeetingState,
+  MediaPermissions,
+} from './MeetingsAdapter';
 export {default as MembershipsAdapter, DestinationType} from './MembershipsAdapter';
 export {default as OrganizationsAdapter} from './OrganizationsAdapter';
 export {default as PeopleAdapter, PersonStatus} from './PeopleAdapter';
